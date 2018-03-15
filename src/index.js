b6e1cd6130e49acd2e815b18d8e5703fef7ec8f9
Chris Diego <chris.diego@dealertrack.com>
2018-03-15 10:26:34 -0400

    s

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import thunk from "redux-thunk";
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
