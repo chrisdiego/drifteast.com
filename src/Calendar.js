6bb031163d281c2e5c26b7a8e73d5605e8a37bb5
Chris Diego <chrisdiego1@gmail.com>
2018-04-28 00:43:58 -0400

    .

--- src/Calendar.js
+++ src/Calendar.js
@@ -2,22 +2,26 @@ import React, { Component } from 'react';
 import EventsList from './EventsList';
 import Loader from './Loader';
 import CalendarSearch from './CalendarSearch';
+import moment from 'moment';
 import s14 from './images/240.jpg';
 
 import './calendar.css';
+
+let today = moment().format();
 
 class Calendar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {loading: false, events: [], value: "", searchResults: []}
+    this.state = {loading: false, events: [], value: "", searchResults: [], showUpcomingEvents: true }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.changeEventFilter = this.changeEventFilter.bind(this);
   }
 
   componentWillMount() {
     this.setState({loading: true});
-    const calendarUrl = "https://www.googleapis.com/calendar/v3/calendars/kambjuls4c2gjbl5kukv1n9cp0@group.calendar.google.com/events?key=AIzaSyAUqQo9YdvNWyzdLPgyNLjDrPsWupw8v0w&singleEvents=true&orderBy=startTime";
+    const calendarUrl = "https://www.googleapis.com/calendar/v3/calendars/kambjuls4c2gjbl5kukv1n9cp0@group.calendar.google.com/events?key=AIzaSyAUqQo9YdvNWyzdLPgyNLjDrPsWupw8v0w&singleEvents=true&orderBy=startTime&timeMin=" + today + "";
     return fetch(calendarUrl)
       .then(response => response.json())
       //searchResults state is also populated with all events as our initial state
@@ -27,6 +31,10 @@ class Calendar extends Component {
 
   handleChange(e) {
     this.setState({value: e.target.value});
+  }
+
+  changeEventFilter() {
+      this.setState({showUpcomingEvents: !this.state.showUpcomingEvents});
   }
 
   handleSubmit(e) {
@@ -41,7 +49,12 @@ class Calendar extends Component {
         <div className="headerImage">
           <div className="headerImageContainer"><h3>Calendar</h3></div>
         </div>
-        <CalendarSearch handleSubmit={this.handleSubmit} handleChange={this.handleChange} value={this.state.value}/>
+        <div className="row">
+          <div className="col-3">
+            Only Show Upcoming Events<button className='btn btn-link' onClick={this.changeEventFilter}><i className={'fas fa-toggle-' + ((this.state.showUpcomingEvents) ? 'on' : 'off')}></i></button>
+          </div>
+          <CalendarSearch handleSubmit={this.handleSubmit} handleChange={this.handleChange} value={this.state.value}/>
+        </div>
         {(this.state.loading) ? <Loader /> : <EventsList searchResults={this.state.searchResults}/>}
       </div>
     )
--- src/Event.js
+++ src/Event.js
@@ -19,7 +19,7 @@ class Event extends Component {
   componentDidMount() {
     setTimeout(() => {
       this.setState({animate: true})
-    }, 150)
+    }, 350)
   }
 
   componentWillReceiveProps(nextProps) {
@@ -32,7 +32,7 @@ class Event extends Component {
   render() {
     const props = this.props;
     return (
-    <div className="col-12 event" style={ { opacity: (this.state.animate) ? 1 : 0, transition: 'opacity 0.2s', transition: 'max-height 1.5s ease-out', maxHeight: (this.state.expanded) ? '1200px' : '175px'} }>
+    <div className="col-12 event" style={ { opacity: (this.state.animate) ? 1 : 0, transition: 'opacity 0.2s, max-height 1.5s ease-out', maxHeight: (this.state.expanded) ? '1200px' : '175px'} }>
         {(this.state.expanded) ? <EventLong event={props.event} expanded={this.state.expanded} onClick={() => this.toggleExpandEvent()} /> : <EventShort event={props.event} expanded={this.state.expanded} onClick={() => this.toggleExpandEvent()} />}
       <div className="row justify-content-end arrow">
         <i className="fas fa-angle-down fa-2x" style={ {transform: (this.state.expanded) ? 'rotate(180deg)' : 'none', transition: 'all 0.5s ease'} }></i>
